77db079fd836a858b732b10d5993f852cd5fd9f3
Malcolm Hays <mah2005atM@gmail.com>
2025-04-22 18:17:02 -0700

    Added comments to revenue_pc.c

--- revenue_pc.c
+++ revenue_pc.c
@@ -6,33 +6,35 @@
 #define DAYS 4
 #define FILE_NAME "revenue_pc_out.txt"
 
-int max(int i1, int i2) {
-    (i1 > i2) ? i1 : i2;
-}
 
 int findMaximumRevenue(int* repairs, int* construction, int i, int days, int* revenue_table, char* choice_table, _Bool* solvedSubproblems) {
+    // Adjust index for array
     int a = i - 1;
     int maxRevenue = 0;
+    // A choice is represented as a character: "c" for construction, "n" for none, and "r" for repairs
     char choice;
     int repairRevenue, constructionRevenue, constructionRevenueToday;
-    _Bool repairsChosen = 0;
 
     // This is so we don't solve the same subproblem more than once
     if (solvedSubproblems[a]) {
-        printf("Day %d: already solved!\n", i);
+        // printf("Day %d: already solved!\n", i);
         return revenue_table[a];
     }
-    printf("Day %d: not solved!\n", i);
+    // printf("Day %d: not solved!\n", i);
 
+    // If this is the first day
     if (i == 1) {
+        // We need to find the maximum of repairs today, construction today, and construction tomorrow
         repairRevenue = repairs[a] + findMaximumRevenue(repairs, construction, i + 1, days, revenue_table, choice_table, solvedSubproblems);
         constructionRevenue = ((i < days) ? construction[a + 1] : 0) + findMaximumRevenue(repairs, construction, i + 2, days, revenue_table, choice_table, solvedSubproblems);
         constructionRevenueToday = construction[a] + findMaximumRevenue(repairs, construction, i + 1, days, revenue_table, choice_table, solvedSubproblems);
 
+        // If repair is the best option
         if (repairRevenue > constructionRevenue && repairRevenue > constructionRevenue) {
             maxRevenue = repairRevenue;
             choice = 'r';
 
+        // If construction tomorrow is the best option
         } else if (constructionRevenue > repairRevenue && constructionRevenue > constructionRevenueToday) {
             maxRevenue = constructionRevenue;
             revenue_table[a + 1] = maxRevenue;
@@ -40,20 +42,25 @@ int findMaximumRevenue(int* repairs, int* construction, int i, int days, int* re
             solvedSubproblems[a + 1] = 1;
             choice = 'n';
 
+        // If construction today is the best option
         } else {
             maxRevenue = constructionRevenueToday;
             choice = 'c';
         }
         
 
+    // If this is not the first day
     } else if (i > 1 && i <= days) {
+        // We need to find the maximum of repairs today and construction tomorrow
         repairRevenue = repairs[a] + findMaximumRevenue(repairs, construction, i + 1, days, revenue_table, choice_table, solvedSubproblems);
         constructionRevenue = ((i < days) ? construction[a + 1] : 0) + findMaximumRevenue(repairs, construction, i + 2, days, revenue_table, choice_table, solvedSubproblems);
 
+        // If repairs today is the best option
         if (repairRevenue > constructionRevenue) {
             maxRevenue = repairRevenue;
             choice = 'r';
 
+        // If construction tomorrow is the best option
         } else {
             maxRevenue = constructionRevenue;
             revenue_table[a + 1] = maxRevenue;
@@ -62,22 +69,26 @@ int findMaximumRevenue(int* repairs, int* construction, int i, int days, int* re
             choice = 'r';
         }
 
+    // If we are out of bounds, we get no revenue
     } else if (i > days) {
         return 0;
     }
 
+    // Set today's revenue and choice, and mark the subproblem as solved
     revenue_table[a] = maxRevenue;
     choice_table[a] = choice;
     solvedSubproblems[a] = 1;
     return maxRevenue;
 }
 
+// For storing an array of ints in a file
 void storeTableInt(FILE* fp, int* table, int numItems) {
     for (int i = 0; i < numItems; i++) {
         fprintf(fp, "%d,", table[i]);
     }
 }
 
+// For storing an array of chars in a file
 void storeTableChar(FILE* fp, char* table, int numItems) {
     for (int i = 0; i < numItems; i++) {
         fprintf(fp, "%c,", table[i]);
